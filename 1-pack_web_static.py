11f180cb81ebcb688ef4c93e71de4e1aa4acc2a3
MutabPato <patrickmtbz@gmail.com>
2024-04-07 06:15:10 -0700

    Compress

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,23 +7,24 @@ folder of your AirBnB Clone repo, using the function do_pack
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """
     function to generate .tgz archive
     """
 
-    #create versions folder if it doesn't exist
+    # create versions folder if it doesn't exist
     local("mkdir -p versions")
 
-    #generate archive name based on current date and time
+    # generate archive name based on current date and time
     now = datetime.now()
     archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
             now.year, now.month, now.day, now.hour, now.minute, now.second)
 
-    #compress content of web_static folder into a .tgz archive
+    # compress content of web_static folder into a .tgz archive
     result = local("tar -cvzf versions/{} web_static".format(archive_name))
 
-    #check if archive was successfully generated
+    # check if archive was successfully generated
     if result.failed:
         return None
     else:
