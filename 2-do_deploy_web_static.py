38d51bab1d9b53829df5b227edc27c6c338694e9
MutabPato <patrickmtbz@gmail.com>
2024-04-08 09:11:11 -0700

    Deploying archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ an archive to your web servers, using the function do_deploy
 from fabric.api import local, put, run, env
 from datetime import datetime
 from os.path import exists
+
 env.hosts = ['52.87.255.220', '3.89.146.3']
 
 
@@ -35,20 +36,33 @@ def do_pack():
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    if not exists(archive_path):
+        print("Archive does not exist.")
         return False
+
     try:
+        # extract necessary information from archive path 
         file_n = archive_path.split("/")[-1]
         no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
+
+        # upload to /tmp/ directory of the web server
         put(archive_path, '/tmp/')
+
+        # create necessary directories and extract archive
         run('mkdir -p {}{}/'.format(path, no_ext))
         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+
+        # clean up and create symbolic link
         run('rm /tmp/{}'.format(file_n))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+
+        print("New version deployed successfully!")
         return True
-    except Exception:
+
+    except Exception as e:
+        print("Error deploying archive:", e)
         return False
